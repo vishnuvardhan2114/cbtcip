dcd7d5cec7878f9caa64c9f96bdc363dd0333397
vishnuvardhan <vishnuvardhan14102002@gmail.com>
2024-03-26 12:59:06 +0530

    db modification on connection

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,21 +1,20 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
+const MONGODB_URI = process.env.MONGODB_URI;
 
-const MONGODB_URL = process.env.MONGODB_URL
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
-let cached = (global as any).mongoose || {conn: null, promise: null};
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
 
-export const connectDatabase = async () => {
-    if (cached.conn) return cached.conn;
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    if(!MONGODB_URL) throw new Error("MONGODB_URL is required");
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently',
+    bufferCommands: false,
+  })
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL,{
-        dbName: "Event Planner360",
-        bufferCommands: false,
-    })
+  cached.conn = await cached.promise;
 
-    cached.conn = await cached.promise;
-
-    return cached.conn;
+  return cached.conn;
 }
