6f390ccedd743a3d0035e544df61979b36411f7d
vishnuvardhan <vishnuvardhan14102002@gmail.com>
2024-03-26 13:36:25 +0530

    modification in db connection problem to db

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from 'clsx'
 
 import { twMerge } from 'tailwind-merge'
-import qs from 'query-string';
+import qs from 'query-string'
 
 import { UrlQueryParams, RemoveUrlQueryParams } from '@/types'
 
