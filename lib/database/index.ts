ca15ee337e071a0bd4ec4a1421acba9bb905fe41
vishnuvardhan <vishnuvardhan14102002@gmail.com>
2024-03-27 19:35:54 +0530

    db

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -5,6 +5,8 @@ const MONGODB_URI = process.env.MONGODB_URI;
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
+  console.log("Connecting to database");
+  
   if (cached.conn) return cached.conn;
 
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
