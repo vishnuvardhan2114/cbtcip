26d2729000223f8ce27f510b705e65fb76572418
vishnuvardhan <vishnuvardhan14102002@gmail.com>
2024-03-28 20:27:46 +0530

    import change

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,11 +2,11 @@
 
 import { revalidatePath } from 'next/cache'
 
-import  {connectToDatabase}  from "../database/index.js";
-import User from "../database/models/user.model.js";
-import Order from "../database/models/order.model.js";
-import Event from "../database/models/event.model.js";
-import { handleError } from "@/lib/utils"
+import { connectToDatabase } from '@/lib/database'
+import User from '@/lib/database/models/user.model'
+import Order from '@/lib/database/models/order.model'
+import Event from '@/lib/database/models/event.model'
+import { handleError } from '@/lib/utils'
 
 import { CreateUserParams, UpdateUserParams } from '@/types'
 
