b01392be7a21adc194b228a1ebd3bc2a46b7dc74
vishnuvardhan <vishnuvardhan14102002@gmail.com>
2024-03-28 21:58:10 +0530

    small change in naming

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,7 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
-import { createUser, deleteUser, updateUser } from '@/lib/actions/user.action'
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { clerkClient } from '@clerk/nextjs'
 import { NextResponse } from 'next/server'
  
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -12,7 +12,7 @@ import { CreateUserParams, UpdateUserParams } from '@/types'
 
 export async function createUser(user: CreateUserParams) {
   try {
-    await connectToDatabase();
+    await connectToDatabase()
 
     const newUser = await User.create(user)
     return JSON.parse(JSON.stringify(newUser))
@@ -49,7 +49,7 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
 
 export async function deleteUser(clerkId: string) {
   try {
-    await connectToDatabase();
+    await connectToDatabase()
 
     // Find user to delete
     const userToDelete = await User.findOne({ clerkId })
