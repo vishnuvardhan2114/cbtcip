62afb550951431547e8a03504b4e1c3cd9200df9
vishnuvardhan <vishnuvardhan14102002@gmail.com>
2024-03-28 20:24:25 +0530

    db test

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,17 +2,17 @@
 
 import { revalidatePath } from 'next/cache'
 
-import { connectToDatabase } from '@/lib/database'
-import User from '@/lib/database/models/user.model'
-import Order from '@/lib/database/models/order.model'
-import Event from '@/lib/database/models/event.model'
-import { handleError } from '@/lib/utils'
+import  {connectToDatabase}  from "../database/index.js";
+import User from "../database/models/user.model.js";
+import Order from "../database/models/order.model.js";
+import Event from "../database/models/event.model.js";
+import { handleError } from "@/lib/utils"
 
 import { CreateUserParams, UpdateUserParams } from '@/types'
 
 export async function createUser(user: CreateUserParams) {
   try {
-    await connectToDatabase()
+    await connectToDatabase();
 
     const newUser = await User.create(user)
     return JSON.parse(JSON.stringify(newUser))
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,12 +1,10 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI 
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-  console.log("Connecting to database");
-  
   if (cached.conn) return cached.conn;
 
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
